7fa0d0f2181fee29ce9d7ed928fdccf09a94fd5f
Davis Foote <davisjfoote@gmail.com>
2023-04-17 19:42:15 -0700

    Remove unfinished value iteration code

--- stealing_gridworld.py
+++ stealing_gridworld.py
@@ -1,8 +1,6 @@
 import gymnasium as gym
 import numpy as np
-import tqdm
 from gymnasium import spaces
-from scipy import sparse
 
 
 class StealingGridworld(gym.Env):
@@ -227,29 +225,6 @@ class StealingGridworld(gym.Env):
                 break
 
         print("Total reward: {}".format(total_reward))
-
-
-def run_value_iteration(
-    sparse_transitions: sparse.csr_matrix,
-    rewards_vector: np.ndarray,
-    horizon: int,
-    gamma: float = 1,
-):
-    num_state_actions, num_states = sparse_transitions.shape
-    num_actions = num_state_actions // num_states
-
-    done_q = np.zeros((num_states, num_actions), dtype=rewards_vector.dtype)
-    done_v = np.zeros(num_states, dtype=rewards_vector.dtype)
-
-    optimal_qs = [done_q]
-    optimal_values = [done_v]
-
-    for t in tqdm.tqdm(list(reversed(list(range(horizon)))), desc="Value iteration"):
-        optimal_qs.insert(
-            0,
-            (rewards_vector + gamma * sparse_transitions @ optimal_values[0]).reshape((num_states, num_actions)),
-        )
-        optimal_values.insert(0, optimal_qs[0].max(axis=1))
 
 
 if __name__ == "__main__":
